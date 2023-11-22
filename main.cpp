ba0279d86963eb6448d45b814beaccd64a2a9a0f
nitesh-19 <niteshgohil25@gmail.com>
2023-11-22 16:36:05 +0530

    Added vector to store faces and draw rectangles

--- main.cpp
+++ main.cpp
@@ -13,6 +13,16 @@ int main() {
     CascadeClassifier faceCascade;
     faceCascade.load(
             R"(C:\\Users\\Nitesh\\Documents\\(B)CommonFiles\\Coding_Workspace\\Face-Detection\\haarcascade_frontalface_default.xml)");
+
+
+    vector<Rect> faces;
+    faceCascade.detectMultiScale(img, faces, 1.1, 10);
+
+    for (int i = 0; i < faces.size(); i++) {
+        rectangle(img, faces[i].tl(), faces[i].br(), Scalar(0, 255, 2), 2);
+
+    }
+
     imshow("Image", img);
     waitKey(0);
     std::cout << "Hello, World!" << std::endl;
