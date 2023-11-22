5acab097b31180e208ab508129dbf2a321a75290
nitesh-19 <niteshgohil25@gmail.com>
2023-11-22 16:11:25 +0530

    Init

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
 
+using namespace cv;
+using namespace std;
 
 
 int main() {
+
     std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
