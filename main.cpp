ea306dbc284553e90939583bfb98b2f12d8aa5f6
nitesh-19 <niteshgohil25@gmail.com>
2023-11-22 21:10:29 +0530

    Added Paho library for MQTT support

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/objdetect.hpp>
+#include "mqtt/async_client.h"
+#include "mqtt/topic.h"
 
 using namespace cv;
 using namespace std;
