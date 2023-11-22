5a1802c0dfc90a217b9262c1df203005970488e7
nitesh-19 <niteshgohil25@gmail.com>
2023-11-22 16:23:05 +0530

    Testing Classifier

--- main.cpp
+++ main.cpp
@@ -8,7 +8,13 @@ using namespace std;
 
 
 int main() {
-
+    string path = R"(C:\\Users\\Nitesh\\Documents\\(B)CommonFiles\\Coding_Workspace\\Face-Detection\\man.jpg)";
+    Mat img = imread(path);
+    CascadeClassifier faceCascade;
+    faceCascade.load(
+            R"(C:\\Users\\Nitesh\\Documents\\(B)CommonFiles\\Coding_Workspace\\Face-Detection\\haarcascade_frontalface_default.xml)");
+    imshow("Image", img);
+    waitKey(0);
     std::cout << "Hello, World!" << std::endl;
 
     return 0;
