675ebef0f1f9a89ce5e05a20fd0d3cb80bac7973
nitesh-19 <niteshgohil25@gmail.com>
2023-11-23 17:21:46 +0530

    Moved mqtt_handler to main, added light toggle with smooth turn on and off upon the detection of eyes

--- main.cpp
+++ main.cpp
@@ -5,10 +5,12 @@
 #include "mqtt/client.h"
 #include "mqtt/async_client.h"
 #include "mqtt/message.h"
+#include<windows.h>
 
 
 using namespace cv;
 using namespace std;
+
 
 
 void mqtt_handler() {
@@ -46,18 +48,81 @@ int main() {
     CascadeClassifier faceCascade;
     vector<Rect> faces;
     Mat img;
-    mqtt_handler();
+    const string server_url = "mqtt://192.168.1.4/1883";
+    const string client_id = "openbkt";
+
+
+    mqtt::async_client my_client(server_url, "");
+    mqtt::topic topic { my_client, "openbkt/2/set", 1};
+
+    try {
+        std::cout << "Connecting to the chat server at '" << server_url
+            << "'..." << std::flush;
+        auto tok = my_client.connect();
+        tok->wait();
+
+        // Subscribe to the topic using "no local" so that
+        // we don't get own messages sent back to us
+
+        std::cout << "Ok\nJoining the group..." << std::flush;
+
+        topic.subscribe()->wait();
+        std::cout << "Ok" << std::endl;
+    }
+    catch (const mqtt::exception& exc) {
+        std::cerr << "\nERROR: Unable to connect. "
+            << exc.what() << std::endl;
+
+    }
+
+    string values[2] = { "0", "60" };
+
     faceCascade.load(
         R"(C:\\Users\\Nitesh\\Documents\\(B)CommonFiles\\Coding_Workspace\\Face-Detection\\haarcascade_eye.xml)");
+    int i = 0;
+
     while (true) {
         webcam.read(img);
-        
-        faceCascade.detectMultiScale(img, faces, 1.1, 5);
-        for (auto &face: faces) {
-            rectangle(img, face.tl(), face.br(), Scalar(0, 255, 2), 2);
+        faceCascade.detectMultiScale(img, faces, 1.1,6);
+        if (!faces.empty()) {
+            for (&i; i <= 60; i++) {
+                topic.publish(to_string(i));
+                Sleep(8);
+            }
+            while (!faces.empty()) {
+               // topic.publish(to_string(60));
+                webcam.read(img);
+                cout << "face" << endl;
+
+                faceCascade.detectMultiScale(img, faces, 1.1, 6);
+
+            }
         }
-        imshow("Webcam", img);
-        waitKey(15);
+        else
+        {
+            for (&i; i >= 0; i--) {
+                topic.publish(to_string(i));
+                Sleep(5);
+            }
+                while (faces.empty()) {
+                   // topic.publish(to_string(0));
+                    webcam.read(img);
+                    cout << "no face" << endl;
+                    faceCascade.detectMultiScale(img, faces, 1.1, 6);
+
+                
+
+
+            }
+
+        }
+        //for (auto &face: faces) {
+       //     rectangle(img, face.tl(), face.br(), Scalar(0, 255, 2), 2);
+        //}
+        //imshow("Webcam", img);
+        //topic.publish(values[i%2]);
+
+        //waitKey(15);
     }
 
 
