78b5c217b345e21e42a206406efb9c849ccd48ed
nitesh-19 <niteshgohil25@gmail.com>
2023-11-23 13:15:58 +0530

    Added MQTT handler

--- main.cpp
+++ main.cpp
@@ -2,24 +2,43 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/objdetect.hpp>
+#include "mqtt/client.h"
 #include "mqtt/async_client.h"
-#include "mqtt/topic.h"
+#include "mqtt/message.h"
+
 
 using namespace cv;
 using namespace std;
 
 
 void mqtt_handler() {
-    const string MQTT_SERVER = "mqtt://192.168.1.4:1883";
-    const int QOS = 1;
-
-    string topic = "openbkt/2/set";
-    auto connOpts = mqtt::connect_options_builder()
-        .keep_alive_interval(20);
-    .clean_session()
-        .finalize();
+    const string server_url = "mqtt://192.168.1.4/1883";
+    const string client_id = "openbkt";
 
 
+    mqtt::async_client my_client(server_url, "");
+    mqtt::topic topic { my_client, "openbkt/2/set", 1};
+
+    try {
+        std::cout << "Connecting to the chat server at '" << server_url
+            << "'..." << std::flush;
+        auto tok = my_client.connect();
+        tok->wait();
+
+        // Subscribe to the topic using "no local" so that
+        // we don't get own messages sent back to us
+
+        std::cout << "Ok\nJoining the group..." << std::flush;
+
+        topic.subscribe()->wait();
+        std::cout << "Ok" << std::endl;
+    }
+    catch (const mqtt::exception& exc) {
+        std::cerr << "\nERROR: Unable to connect. "
+            << exc.what() << std::endl;
+       
+    }
+    topic.publish("50");
 }
 
 int main() {
@@ -27,11 +46,12 @@ int main() {
     CascadeClassifier faceCascade;
     vector<Rect> faces;
     Mat img;
-
+    mqtt_handler();
+    faceCascade.load(
+        R"(C:\\Users\\Nitesh\\Documents\\(B)CommonFiles\\Coding_Workspace\\Face-Detection\\haarcascade_eye.xml)");
     while (true) {
         webcam.read(img);
-        faceCascade.load(
-                R"(C:\\Users\\Nitesh\\Documents\\(B)CommonFiles\\Coding_Workspace\\Face-Detection\\haarcascade_eye.xml)");
+        
         faceCascade.detectMultiScale(img, faces, 1.1, 5);
         for (auto &face: faces) {
             rectangle(img, face.tl(), face.br(), Scalar(0, 255, 2), 2);
